4aacc1ecb4c804424f9a2e18e3fd03501e4b1aed
Daniel Martínez <danielmartinezalmela@gmail.com>
2017-11-12 21:03:20 +0100

    Nuevos logos para rooty

--- header.php
+++ header.php
@@ -24,7 +24,7 @@
     <header>
       <div class="container">
         <!--<h1><?php bloginfo('name'); ?></h1>-->
-        <img src="https://raw.githubusercontent.com/DanMnez/Rooty/master/library/images/logo_rooty.png" id="logo" alt="Rooty">
+        <img src="https://raw.githubusercontent.com/DanMnez/Rooty/master/library/images/logo_rooty_light.png" id="logo" alt="Rooty">
         <nav class="main-nav">
             <?php wp_nav_menu( array( 'theme_location' => 'navigation' ) ); ?>
         </nav>
